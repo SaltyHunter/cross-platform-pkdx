78aef79f14a745ccc570aec4caf63a7428711fd6
SaltyHunter <kramoreau@gmail.com>
2020-02-29 22:18:29 +0100

    Alpha 0.2

--- Signout.js
+++ Signout.js
@@ -33,8 +33,6 @@ export default class Logout extends React.Component {
 
 const styles = StyleSheet.create({
     container: {
-        flex : 1,
-        justifyContent : "center",
-        alignItems: "center",
+        flex:1
     }
 });
